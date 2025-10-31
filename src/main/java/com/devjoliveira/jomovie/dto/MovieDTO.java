dfd4c87dc0598556fb6088c53c33a459de5b614f
devjoliveira <macpro@MacBook-Pro-de-MacPro.local>
2025-10-30 22:49:32 -0300

    Create a scheme in movie dto to description the variable

--- src/main/java/com/devjoliveira/jomovie/dto/MovieDTO.java
+++ src/main/java/com/devjoliveira/jomovie/dto/MovieDTO.java
@@ -8,6 +8,7 @@ import org.hibernate.validator.constraints.URL;
 
 import com.devjoliveira.jomovie.entities.MovieEntity;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.PositiveOrZero;
 import jakarta.validation.constraints.Size;
@@ -16,18 +17,23 @@ public class MovieDTO {
 
 	private static final DecimalFormat df = new DecimalFormat("#.##", new DecimalFormatSymbols(Locale.US));
 
+	@Schema(description = "Database generated movie id")
 	private Long id;
 
+	@Schema(description = "Movie title")
 	@NotBlank(message = "Required field")
 	@Size(min = 5, max = 80, message = "Title must be between 5 and 80 characters")
 	private String title;
 
+	@Schema(description = "Score average of the movie")
 	@PositiveOrZero(message = "Score should be greater than or equal to zero")
 	private Double score;
 
+	@Schema(description = "Number of scores received for the movie")
 	@PositiveOrZero(message = "Count should be greater than or equal to zero")
 	private Integer count;
 
+	@Schema(description = "Movie image URL")
 	@NotBlank(message = "Required field")
 	@URL(message = "Field must be a valid url")
 	private String image;
